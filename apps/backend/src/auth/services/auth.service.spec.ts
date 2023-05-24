97ff937499159b114b1e8094c663ee8c6721fe11
osyukov.v <osyukov.v@ideadlt.com>
2023-05-24 23:54:27 +0300

    test: add test

--- apps/backend/src/auth/services/auth.service.spec.ts
+++ apps/backend/src/auth/services/auth.service.spec.ts
@@ -2,32 +2,46 @@ import { Test } from '@nestjs/testing';
 
 import { UserService } from '../../user/services/user.service';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from '../../user/entities/user.entity';
 
-describe('dssdffd sdff', () => {
-  console.log('userService');
+describe('AuthService', () => {
   let authService: AuthService;
   let userService: UserService;
+  let jwtService: JwtService;
+
   beforeEach(async () => {
-    console.log('userService');
     const module = await Test.createTestingModule({
-      providers: [AuthService, { provide: UserService, useValue: {} }],
+      providers: [AuthService, { provide: UserService, useValue: { getUser: jest.fn(), register: jest.fn() } }, { provide: JwtService, useValue: { sign: jest.fn() } }],
     }).compile();
 
     authService = module.get(AuthService);
     userService = module.get(UserService);
-
-    console.log(userService);
+    jwtService = module.get(JwtService);
   });
 
-  describe('sdf', () => {
-    let result;
+  describe('register', () => {
+    let result: string;
+
     beforeEach(async () => {
-      // jest.spyOn(userService, 'getUser').mockResolvedValueOnce({} as never)
+      jest.spyOn(userService, 'register').mockResolvedValueOnce({ id: 'id', email: 'email' } as UserEntity);
+      jest.spyOn(jwtService, 'sign').mockReturnValueOnce('sign');
+
       result = await authService.register('test@test.test', 'pas');
     });
 
-    it('fsd', () => {
-      expect(result).toStrictEqual(1);
+    it('must register user', () => {
+      expect(userService.register).toHaveBeenCalledTimes(1);
+      expect(userService.register).toHaveBeenCalledWith('test@test.test', 'pas');
+    });
+
+    it('must sign payload', () => {
+      expect(jwtService.sign).toHaveBeenCalledTimes(1);
+      expect(jwtService.sign).toHaveBeenCalledWith({ email: 'email', userId: 'id' }, { expiresIn: '30d' });
+    });
+
+    it('must return token', () => {
+      expect(result).toBe('sign');
     });
   });
 });

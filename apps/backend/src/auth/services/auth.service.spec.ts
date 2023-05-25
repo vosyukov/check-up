459aa6e77fed55a393d9f3517c904ea6c49f8be6
osyukov.v <osyukov.v@ideadlt.com>
2023-05-25 23:46:11 +0300

    test: add test

--- apps/backend/src/auth/services/auth.service.spec.ts
+++ apps/backend/src/auth/services/auth.service.spec.ts
@@ -44,4 +44,29 @@ describe('AuthService', () => {
       expect(result).toBe('sign');
     });
   });
+
+  describe('login', () => {
+    let result: string;
+
+    beforeEach(async () => {
+      jest.spyOn(userService, 'getUser').mockResolvedValueOnce({ id: 'id', email: 'email', password:'pas' } as UserEntity);
+      jest.spyOn(jwtService, 'sign').mockReturnValueOnce('sign');
+
+      result = await authService.login('test@test.test', 'pas');
+    });
+
+    it('must fetch user', () => {
+      expect(userService.getUser).toHaveBeenCalledTimes(1);
+      expect(userService.getUser).toHaveBeenCalledWith({"email": "test@test.test"});
+    });
+
+    it('must sign payload', () => {
+      expect(jwtService.sign).toHaveBeenCalledTimes(1);
+      expect(jwtService.sign).toHaveBeenCalledWith({ email: 'email', userId: 'id' }, { expiresIn: '30d' });
+    });
+
+    it('must return token', () => {
+      expect(result).toBe('sign');
+    });
+  })
 });

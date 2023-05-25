08177cbfb5b3879d7cab81732223bf0decf6a051
Vadim <osyukov.vadim@gmail.com>
2023-05-25 23:47:27 +0300

    Add test (#4)
    * test: add test
    
    * build: fix build
    
    * test: add test
    
    ---------
    
    Co-authored-by: osyukov.v <osyukov.v@ideadlt.com>

--- apps/backend/src/auth/services/auth.service.ts
+++ apps/backend/src/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, Injectable} from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../../user/services/user.service';

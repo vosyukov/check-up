3df513ea052cb2535d60c3cbde0a1208df2b28fa
osyukov.v <osyukov.v@ideadlt.com>
2023-05-26 23:59:11 +0300

    fix: fix email notification

--- apps/backend/src/ping-handler/services/ping-handler.service.ts
+++ apps/backend/src/ping-handler/services/ping-handler.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { PingEntity } from '../entities/ping.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CheckObjectService, CheckObjectStatus } from '../../check-object/services/check-object.service';
-import { EmailNotificationService } from '../../notification/services/email-notification.service';
+import {Injectable} from '@nestjs/common';
+import {Repository} from 'typeorm';
+import {PingEntity} from '../entities/ping.entity';
+import {InjectRepository} from '@nestjs/typeorm';
+import {CheckObjectService, CheckObjectStatus} from '../../check-object/services/check-object.service';
+import {EmailNotificationService} from '../../notification/services/email-notification.service';
+import {UserService} from "../../user/services/user.service";
 
 @Injectable()
 export class PingHandlerService {
@@ -11,8 +12,10 @@ export class PingHandlerService {
     @InjectRepository(PingEntity)
     private readonly pingRepository: Repository<PingEntity>,
     private readonly checkObjectService: CheckObjectService,
-    private readonly emailNotificationService: EmailNotificationService
-  ) {}
+    private readonly emailNotificationService: EmailNotificationService,
+    private readonly userService: UserService
+  ) {
+  }
 
   public async handleOk(checkObjectId: string): Promise<string> {
     const checkObject = await this.checkObjectService.getCheckObject(checkObjectId);
@@ -28,8 +31,13 @@ export class PingHandlerService {
         lastPingTimestamp: timestamp,
         status: CheckObjectStatus.UP,
       });
+      const user = await this.userService.getUser({id: checkObject.id})
 
-      await this.emailNotificationService.sendNotification(checkObject.userId, checkObject.id, CheckObjectStatus.UP);
+      await this.emailNotificationService.sendNotification({
+        checkName: checkObject.name,
+        status: CheckObjectStatus.UP,
+        email: user.email
+      });
     } else {
       await this.checkObjectService.updateCheckObject(checkObject.userId, {
         id: checkObject.id,
